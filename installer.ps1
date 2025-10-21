9e5128b48692ee1d31053d61d34c50463bff3fde
R. S. Doiel <rsdoiel@gmail>
2025-10-21 09:52:31 -0700

    Quick Save

--- installer.ps1
+++ installer.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
-# generated with CMTools 0.0.1 c7de007
+# generated with CMTools 0.0.1 85a1222
 
 #
 # Set the package name and version to install
--- patentfetch.1.md
+++ patentfetch.1.md
@@ -1,4 +1,4 @@
-%patentfetch(1) user manual | version 0.0.1 c7de007
+%patentfetch(1) user manual | version 0.0.1 85a1222
 % R. S. Doiel
 % 2025-10-21
 
